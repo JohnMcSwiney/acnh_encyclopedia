fbaae1e9ab68525c535629d249ee3beb3c88839d
Johnny Mcswiney <102752692+JohnMcSwiney@users.noreply.github.com>
2025-05-13 22:55:25 -0600

    Updated dependancies and worked on merging bug data and image urls

--- server/bug_merge.py
+++ server/bug_merge.py
@@ -12,23 +12,47 @@ with open(json_file_path, 'r', encoding='utf-8') as json_file:
 bugs_info = []
 
 # for entry in data_list:
-    # Access the "urls" key from each dictionary
-    # print(data_list.urls)
+# Access the "urls" key from each dictionary
+# print(data_list.urls)
+
+# bug_name_1 > removing the "_NH_Icon.png"
+# bug_name_2 > removing the "120px-"
+# bug_name_3 > replace "_" with " "
 for url in urls:
         # Parse the URL and get the filename
         filename = urlparse(url).path.split('/')[-1]
-
+        
         # Extract name from the filename
-        name = unquote(filename.split('_NH_Icon.png')[0])
+        bug_name_1 = unquote(filename.split('_NH_Icon.png')[0])
+        print(bug_name_1)
+        
+        try:
+            bug_name_2 = unquote(bug_name_1.split('120px-')[1])
+            print(bug_name_2)
+        except:
+            print("|!| - Unable strip: " + bug_name_1 + " any further")
+            bug_name_2 = bug_name_1
 
-        # Create a dictionary with name and URL
-        bug_entry = {"name": name, "url": url}
-        # print(bug_entry)
+        #Replace the underscores in the bug-names with spaces 
+        #Attempt and handle errors from text without _
+        try:
+            bug_name_3 = bug_name_2.replace("_", " ", 3)
+        except:
+            bug_name_3 = bug_name_2
+
+        # Create a dictionary with name and URL,
+        # Catch potential errors
+        try:
+            bug_entry = {"name": bug_name_3, "url": url}
+        
+        except:
+             print("|!| - Unable to combine Entry and URL")
+
         bugs_info.append(bug_entry)
-        # print(bug_entry)
+
 
 # Create a new JSON file with the extracted information
-output_json_file_path = 'bug_images.json'
+output_json_file_path = 'bug_images_may2025-test.json'
 with open(output_json_file_path, 'w', encoding='utf-8') as json_output_file:
     json.dump(bugs_info, json_output_file, ensure_ascii=False, indent=2)
 
